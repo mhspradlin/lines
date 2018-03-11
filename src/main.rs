8aaca8835c889e345b873852a23e795ed1abf7cb
Mitch Spradlin <mitch@spradlin.net>
2018-03-11 10:09:49 -0700

    Configured and tested log line formatting and file compression+rollover

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 #[macro_use] extern crate log;
 extern crate log4rs;
-use std::env;
 
 fn main() {
-    let path = env::current_dir().expect("Failure opening current dir");
-    println!("Current dir: {}", path.display());
-
     log4rs::init_file("config/log4rs.yml", Default::default()).unwrap();
     info!("Hello, world!");
 }
