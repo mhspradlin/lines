3205d8982cac1fd3fcb4ef170196cb67e4b230c5
Mitch Spradlin <mitch@spradlin.net>
2018-03-12 07:10:14 -0700

    Light refactoring a not waiting for potentially negative times

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate rayon;
 
 use std::time::{Duration, Instant};
 use rayon::prelude::*;
+use rayon::ThreadPool;
 use std::thread;
 
 fn main() {
@@ -13,19 +14,13 @@ fn main() {
     let update_interval = Duration::from_secs(1);
     let num_sensors = 10;
 
+    let sensor_pool = make_sensor_thread_pool(num_sensors as usize);
     let sensors = make_dummy_sensors(num_sensors);
-    let sensor_pool = rayon::ThreadPoolBuilder::new()
-                        .num_threads(num_sensors as usize)
-                        .thread_name(|thread_index| "sensor-pool-thread-".to_string() + &thread_index.to_string())
-                        .build()
-                        .unwrap();
     let mut last_update = Instant::now();
     loop {
-        sensor_pool.install(||
-            sensors.par_iter()
-                   .for_each(|ref sensor| sensor.sense()));
-        let time_until_next_update = update_interval - Instant::now().duration_since(last_update);
-        thread::sleep(time_until_next_update);
+        debug!("Running all sensors in parallel");
+        run_all_sensors_in_parallel(&sensor_pool, &sensors);
+        sleep_until_target_time(last_update, update_interval);
         last_update = Instant::now();
     }
 }
@@ -48,4 +43,30 @@ fn make_dummy_sensors(number: u8) -> Vec<DummySensor> {
         dummy_sensors.push(DummySensor("Sensor".to_string() + &index.to_string()));
     }
     return dummy_sensors;
+}
+
+fn make_sensor_thread_pool(num_sensors: usize) -> ThreadPool {
+    rayon::ThreadPoolBuilder::new()
+                        .num_threads(num_sensors as usize)
+                        .thread_name(|thread_index| "sensor-pool-thread-".to_string() + &thread_index.to_string())
+                        .build()
+                        .unwrap()
+}
+
+fn run_all_sensors_in_parallel(sensor_pool: &ThreadPool, sensors: &Vec<DummySensor>) {
+    sensor_pool.install(||
+        sensors.par_iter()
+                .for_each(|ref sensor| sensor.sense()))
+}
+
+fn sleep_until_target_time(last_wakeup: Instant, target_interval: Duration) {
+    let time_until_next_wakeup = target_interval - Instant::now().duration_since(last_wakeup);
+    debug!("Time until next wakeup: {:.3}s", duration_in_seconds(&time_until_next_wakeup));
+    if time_until_next_wakeup > Duration::from_millis(0) {
+        thread::sleep(time_until_next_wakeup);
+    }
+}
+
+fn duration_in_seconds(duration: &Duration) -> f64 {
+    duration.as_secs() as f64 + duration.subsec_nanos() as f64 / 1_000_000_000 as f64
 }
