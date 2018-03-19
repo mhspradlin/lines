24f6b97a361dfb9d447fea12f5e3260ccf90eee5
Mitch Spradlin <mitch@spradlin.net>
2018-03-18 19:31:16 -0700

    Layout for Linux RAM space usage

--- src/sensors/physical_memory.rs
+++ src/sensors/physical_memory.rs
@@ -55,6 +55,45 @@ mod platform {
     }
 }
 
+#[cfg(target_os="linux")]
+mod platform {
+    extern crate libc;
+
+    use super::Sensor;
+    use super::PhysicalMemorySensor;
+    use cadence::prelude::*;
+    use cadence::StatsdClient;
+    use std::mem;
+    use std::io::Error;
+
+    const FALSE: i32 = 0;
+    const FATAL_ERROR: &'static str = "Fatal error counting metric";
+    const METRICS_PREFIX: &'static str = "drive";
+    lazy_static! {
+        static ref TOTAL_BYTES: String = METRICS_PREFIX.to_string() + ".total_bytes";
+        static ref FREE_BYTES: String = METRICS_PREFIX.to_string() + ".free_bytes";
+    }
+
+    impl Sensor for PhysicalMemorySensor {
+        fn sense(&self, statsd_client: &StatsdClient) {
+            let mut info_struct: libc::sysinfo = unsafe { mem::zeroed() };
+            let return_code = unsafe { libc::sysinfo(&mut info_struct as *mut libc::sysinfo) };
+            if return_code == FALSE {
+                let total_accessible_bytes = info_struct.totalram * info_struct.mem_unit;
+                let total_free_bytes = info_struct.freeram * info_struct.mem_unit;
+                info!("Total accessible physical memory: {} MiB", total_accessible_bytes / 1024 / 1024);
+                info!("Total free physical memory: {} MiB", total_free_bytes / 1024 / 1024);
+                statsd_client.count(&TOTAL_BYTES, super::value_or_max(total_accessible_bytes))
+                    .expect(FATAL_ERROR);
+                statsd_client.count(&FREE_BYTES, super::value_or_max(total_free_bytes))
+                    .expect(FATAL_ERROR);
+            } else {
+                error!("Error getting physical memory usage: {}", Error::last_os_error());
+            }
+        }
+    }
+}
+
 fn value_or_max(value: u64) -> i64 {
     if value >= i64::MAX as u64 {
         warn!("Value {} larger than max value of {}, reporting max value {} instead",
